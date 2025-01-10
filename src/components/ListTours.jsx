0421155a5abb82f6c7428b3b807bf073579e4c87
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-01-10 17:05:57 +0300

    add alert error

--- src/components/ListTours.jsx
+++ src/components/ListTours.jsx
@@ -31,7 +31,27 @@ const ListTours = () => {
         Our Tours
       </h1>
       <div className="w-24 h-1 rounded-sm place-self-center bg-green-600"></div>
-      {error && <p>error</p>}
+      {error && (
+        <div
+          class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50  dark:text-red-400"
+          role="alert"
+        >
+          <svg
+            class="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">Danger alert!</span> Change a few things
+            up and try submitting again.
+          </div>
+        </div>
+      )}
       {tours.length === 0 && (
         <>
           <div className="flex flex-col">
