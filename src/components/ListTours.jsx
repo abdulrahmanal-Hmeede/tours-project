58b17d68c11e36057b76c0a76b2daa557eee73ec
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-01-10 17:02:10 +0300

    add handelRefresh

--- src/components/ListTours.jsx
+++ src/components/ListTours.jsx
@@ -10,11 +10,19 @@ const ListTours = () => {
     setTours(tours.filter((tour) => tour.id !== id));
   };
 
-  useEffect(() => {
+  const getData = () => {
     apiClient
       .get("/react-tours-project")
       .then((res) => setTours(res.data))
       .catch((err) => setError(err.message));
+  };
+
+  const handelRefresh = () => {
+    getData();
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
 
   return (
@@ -25,12 +33,22 @@ const ListTours = () => {
       <div className="w-24 h-1 rounded-sm place-self-center bg-green-600"></div>
       {error && <p>error</p>}
       {tours.length === 0 && (
-        <div
-          class="p-4 mb-4 text-md text-center mt-5 text-green-800 rounded-lg bg-green-50  dark:text-green-400"
-          role="alert"
-        >
-          <span class="font-medium text-4xl ">No Tours Lift</span>
-        </div>
+        <>
+          <div className="flex flex-col">
+            <div
+              class="p-4 mb-4 text-md text-center mt-5 text-green-800 rounded-lg bg-green-50  dark:text-green-400"
+              role="alert"
+            >
+              <span class="font-medium text-4xl ">No Tours Lift</span>
+            </div>
+            <button
+              className="transition ease-in-out bg-green-500 w-24 place-self-center text-white p-2 rounded-md hover:bg-green-600"
+              onClick={() => handelRefresh()}
+            >
+              Refresh
+            </button>
+          </div>
+        </>
       )}
       <div className="grid grid-cols-3 gap-14 place-items-center p-10">
         {tours.map((tour) => (
